c62ea35db0c82da1b8e6726edd9a560fa0933136
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-01-18 23:58:28 +0200

    :star: bookmarks: utilize context =>  toggle bookmark in list and jobItem

--- src/components/BookmarkIcon.tsx
+++ src/components/BookmarkIcon.tsx
@@ -1,9 +1,21 @@
 import { BookmarkFilledIcon } from "@radix-ui/react-icons";
+import { useBookmarkContext } from "../hooks/useBookmarkContext";
 
-export default function BookmarkIcon() {
+export default function BookmarkIcon({ id }: { id: number }) {
+  const { bookmarkedIds, handleToggleBookmark } = useBookmarkContext();
+
   return (
-    <button className="bookmark-btn">
-      <BookmarkFilledIcon className="filled" />
+    <button
+      onClick={(e) => {
+        handleToggleBookmark(id);
+        e.stopPropagation();
+        e.preventDefault();
+      }}
+      className="bookmark-btn"
+    >
+      <BookmarkFilledIcon
+        className={bookmarkedIds.includes(id) ? "filled" : ""}
+      />
     </button>
   );
 }
