907fa06e9572d51436fe13ec5b6fbe07bc2959ce
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-01-19 19:32:31 +0200

    💎 impr: createPortal for popover

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,14 +1,16 @@
 import { forwardRef } from "react";
 import { useBookmarkContext } from "../hooks/useBookmarkContext";
 import JobList from "./JobList";
+import { createPortal } from "react-dom";
 
 const BookmarksPopover = forwardRef<HTMLDivElement>(function (_, ref) {
   const { bookmarkedJobItems, isLoading } = useBookmarkContext();
 
-  return (
+  return createPortal(
     <div ref={ref} className="bookmarks-popover">
       <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />
-    </div>
+    </div>,
+    document.body
   );
 });
 
